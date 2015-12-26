b6127086760e8c84705593e69fe9904a92465901
Purlox <purlox@gmail.com>
2015-12-26 18:09:31 +0100

    Changed the type Chance to Probability.
    Same reason for change as the previous commit's.

--- Source/Data/Probability.hs
+++ Source/Data/Probability.hs
@@ -1,6 +1,6 @@
-module Data.Chance 
-( Chance(..)
-, chanceToRational
+module Data.Probability 
+( Probability(..)
+, probabilityToRational
 , getPercentage
 , reduce
 , getInverse
@@ -11,46 +11,46 @@ import Data.Function (on)
 import Data.Monoid ((<>))
 
 
--- | Represents the chance of something happening. Shouldn't get higher than 1.
-data Chance = Int :/: Int
+-- | Represents the probability of something happening. Shouldn't get higher than 1.
+data Probability = Int :/: Int
     deriving (Show)
 
 
-instance Eq Chance where
+instance Eq Probability where
     (==) = equals `on` reduce
         where equals (a :/: b) (c :/: d) = a == c && b == d
 
 
-instance Ord Chance where
+instance Ord Probability where
     compare = compare2 `on` reduce
         where compare2 (a :/: b) (c :/: d) = compare x y
                   where x = a * d
                         y = c * b
 
 
--- | Converts Chance to Rational number.
-chanceToRational :: Chance -> Rational
-chanceToRational (x :/: y) = toInteger x Ratio.% toInteger y
+-- | Converts Probability to Rational number.
+probabilityToRational :: Probability -> Rational
+probabilityToRational (x :/: y) = toInteger x Ratio.% toInteger y
 
 
--- | Converts Chance to a percentage.
-getPercentage :: Chance -> Double
+-- | Converts Probability to a percentage.
+getPercentage :: Probability -> Double
 getPercentage (x :/: y) = 100 * fromIntegral x / fromIntegral y
 
 
--- | Reduces the Chance's fraction to its irreducible form.
+-- | Reduces the Probability's fraction to its irreducible form.
 -- 
 -- Especially useful to do before comparing two fractions or before printing them.
-reduce :: Chance -> Chance
+reduce :: Probability -> Probability
 reduce (x :/: y) = x' :/: y'
     where commonDivisor = gcd x y
           x' = x `div` commonDivisor
           y' = y `div` commonDivisor
 
 
--- | Returns inverse chance to the given one.
+-- | Returns inverse Probability to the given one.
 --
--- In other words, if the original Chance was reflecting the chance that P happens, then the inverse one reflects the chance of P not happening.
-getInverse :: Chance -> Chance
+-- In other words, if the original Probability was of P happening, then the inverse Probability reflects P not happening.
+getInverse :: Probability -> Probability
 getInverse (x :/: y) = (y - x) :/: y
 
